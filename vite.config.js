fd3de27d0294077cb20960f90c9d30c810bb6dfa
ParthRaval20 <parthravalp20p@gmail.com>
2026-05-02 19:39:20 +0530

    Add Google site verification file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sitemap } from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://gayatristeel.vercel.app',
+      outDir: 'public',
+      filename: 'sitemap.xml',
+      lastmod: new Date().toISOString().split('T')[0],
+      priority: 1.0,
+    }),
+  ],
 })
